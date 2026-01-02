c3206f68b5d655fcf3f9fc169f6c882d2658c122
TEZ TANMAY PANDAB <teztanmay5032555@gmail.com>
2026-01-02 18:16:09 +0530

    day 1 condtitionals

---
+++ house.py
@@ -0,0 +1,8 @@
+name = input("whats your name?")
+
+if name == "harry" or name =="heromine" or name == "ron":
+    print("griffindor")
+elif name == "tez":
+    print("India")
+else:
+    print("who?")
